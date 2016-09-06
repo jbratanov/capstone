a38e47437cbd74c7ef5e2ba0d9c3a650d964567c
jbratanov <john.bratanov@verizon.com>
2016-09-06 15:28:42 -0500

    save and read scripts to make life easier

---
+++ readRDS_project.R
@@ -0,0 +1,24 @@
+
+
+# 2-gram
+df.2 <- readRDS("c:/Coursera/Capstone/data/df.2.rds")
+
+dfm.2 <- readRDS("c:/Coursera/Capstone/data/dfm.2.rds")
+
+fit.2 <- readRDS("c:/Coursera/Capstone/data/fit.2.rds")
+
+words.2 <- readRDS("c:/Coursera/Capstone/data/words.2.rds")
+
+freqTerms.2 <- readRDS("c:/Coursera/Capstone/data/freqTerms.2.rds")
+
+
+# 3-gram
+df.3 <- readRDS("c:/Coursera/Capstone/data/df.3.rds")
+
+dfm.3 <- readRDS("c:/Coursera/Capstone/data/dfm.3.rds")
+
+fit.3 <- readRDS("c:/Coursera/Capstone/data/fit.3.rds")
+
+words.3 <- readRDS("c:/Coursera/Capstone/data/words.3.rds")
+
+freqTerms.3 <- readRDS("c:/Coursera/Capstone/data/freqTerms.3.rds")
---
+++ saveRDSFiles.R
@@ -0,0 +1,23 @@
+
+# 2-gram
+saveRDS(df.2, "c:/Coursera/Capstone/data/df.2.rds")
+
+saveRDS(dfm.2, "c:/Coursera/Capstone/data/dfm.2.rds")
+
+saveRDS(fit.2, "c:/Coursera/Capstone/data/fit.2.rds")
+
+saveRDS(words.2, "c:/Coursera/Capstone/data/words.2.rds")
+
+saveRDS(freqTerms.2, "c:/Coursera/Capstone/data/freqTerms.2.rds")
+
+
+# 3-gram
+saveRDS(df.3, "c:/Coursera/Capstone/data/df.3.rds")
+
+saveRDS(dfm.3, "c:/Coursera/Capstone/data/dfm.3.rds")
+
+saveRDS(fit.3, "c:/Coursera/Capstone/data/fit.3.rds")
+
+saveRDS(words.3, "c:/Coursera/Capstone/data/words.3.rds")
+
+saveRDS(freqTerms.3, "c:/Coursera/Capstone/data/freqTerms.3.rds")
