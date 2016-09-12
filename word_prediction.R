8d9b9831a60896031b5582d7d6b542a76e7dd755
jbratanov <john.bratanov@verizon.com>
2016-09-12 16:46:38 -0500

    update changes

--- word_prediction.R
+++ word_prediction.R
@@ -16,8 +16,8 @@
 #-------------------------------------------------------------------------------------------------------
 
 # allows for easier changes during testing
-sampleBlogPct <- 0.01
-sampleNewsPct <- 0.01
+sampleBlogPct <- 0.7
+sampleNewsPct <- 0.7
 sampleTwitPct <- 0.001
 # Program directories
 appDir="c:/Coursera/Capstone/"
@@ -39,6 +39,13 @@ sampleText <- c(sample(blogText,length(blogText)*sampleBlogPct),
 # Convert text to ASCII.  Removes special non-english characters
 sampleText <- iconv(sampleText, "UTF-8", "ASCII", sub = "")
 
+# Save sample text file to file
+saveRDS(sampleText, (paste0(dataDir,"sampleText.rds")))
+
+# Delete RFiles to save memory
+rm(blogText, newsText, twitterText)
+
+
 # Create train and testing sets
 #library(caTools) # Needed for split
 #splitSample = sample.split(sampleText, 0.7) 
@@ -50,15 +57,11 @@ sampleText <- iconv(sampleText, "UTF-8", "ASCII", sub = "")
 #-------------------------------------------------------------------------------------------------------
 # I decided to try quanteda.  I found the R "tm" package slow, too much memory and complex.
 # Documenting along the way and trying to compare what I did using the "tm" package.
-
-
 # Create the Corpus
 # A corpus is a kind of vector/list that facilitates applying text processing commands
 # to many texts at once.
 
-# put them into a corpus
 require(quanteda)
-sampleCorpus <- corpus(sampleText)
 
 # Convert the text to data
 # by creating a document-term matrix where each row is a document and each column is a word
@@ -67,29 +70,53 @@ sampleCorpus <- corpus(sampleText)
 # Remove features.  This is what's used as "tm_map" in "tm"
 # concatenator option defaults to "-".  Need space for doing word predictor functionality
 # removeTwitter option TRUE removes "#" and "@" characters
+
+#######################################
+# Get 2-ngram corpus data
+#######################################
+
+# Create Doc Term Matrix for 2-ngram
 dfm.2 <- dfm(sampleText, ngrams=2,
              toLower = TRUE,concatenator = " ", removeNumbers = TRUE, removeSymbols=TRUE,
              removePunct = TRUE, removeSeparators = TRUE, removeTwitter = TRUE, removeURL=TRUE)
 
+# Save dfm n-gram data to file
+saveRDS(dfm.2, (paste0(dataDir,"dfm.2.rds")))
+
+# Get frequency for each unigram
+freqTerms.2 <- topfeatures(dfm.2, n = nfeature(dfm.2))
+freqTerms.2 <-freqTerms.2[freqTerms.2 > 5]
+
+# Save corpus freqTerms data to file
+saveRDS(freqTerms.2, (paste0(dataDir,"freqTerms.2.rds")))
+
+# Delete RFiles to save memory
+rm(dfm.2)
+
+#######################################
+# Get 3-ngram corpus data
+#######################################
+
+# Create Doc Term Matrix for 3-ngram
 dfm.3 <- dfm(sampleText, ngrams=3,
              toLower = TRUE,concatenator = " ", removeNumbers = TRUE, removeSymbols=TRUE,
              removePunct = TRUE, removeSeparators = TRUE, removeTwitter = TRUE, removeURL=TRUE)
 
-#-------------------------------------------------------------------------------------------------------
-#  Get top features,
-#  Drop frequency > 1
-#-------------------------------------------------------------------------------------------------------
-
-# 2-ngram
-freqTerms.2 <- topfeatures(dfm.2, n = nfeature(dfm.2))
-freqTerms.2 <-freqTerms.2[freqTerms.2 > 5]
-
-# 3-ngram.  Create file of dropped data for good turing smoothing model
+# Get frequency for each bigram.
+# Create file of dropped data for good turing smoothing model
 freqTerms.3 <- topfeatures(dfm.3, n = nfeature(dfm.3))
-df.3.dropped <-freqTerms.3[freqTerms.3 < 6]
-df.3.drop <- data.frame(word=names(df.3.dropped), freq=df.3.dropped)
-# Drop frequency > 5
-freqTerms.3 <-freqTerms.3[freqTerms.3 > 5]
+#df.3.dropped <-freqTerms.3[freqTerms.3 < 6]  # Drop frequency > 5 to drop file
+#df.3.drop <- data.frame(word=names(df.3.dropped), freq=df.3.dropped)
+freqTerms.3 <-freqTerms.3[freqTerms.3 > 5] # Keep freqency > 5 to word prediction file
+
+# Save corpus freqTerms data to file
+saveRDS(freqTerms.3, (paste0(dataDir,"freqTerms.3.rds")))
+
+# Delete RFiles to save memory
+rm(dfm.3)
+
+# Delete raw data sample text RFile to save memory
+rm(sampleText)
 
 
 #-------------------------------------------------------------------------------------------------------
@@ -118,6 +145,9 @@ unigram <- word(df.3$word,1,1)
 df.3$bigram <- cbind(bigram)
 df.3$unigram <- cbind(unigram)
 
+# Delete RFiles to save memory
+rm(unigram, bigram)
+
 #####################################################################
 # 3-ngram keys for trie prefix trees
 # Used "|" as separator to create uniqueness in duplicate keys
@@ -128,26 +158,18 @@ for (i in length(df.3$bigram):1)
 # bind key data to 3-ngram DF
 df.3$key <- cbind(key)
 
+# Delete RFiles to save memory
+rm(key)
 #-------------------------------------------------------------------------------------------------------
-#  Create Markov Chain
+# Load trie data structures
 #-------------------------------------------------------------------------------------------------------
-#load markovchain package
-library(markovchain)
-library(tm)
 
-# Create data required for markov chain fit
-# I couldn't find a markovchain function for using frequenices, so created raw data to fit
-words.3 <- rep(df.3$word, df.3$freqTerms.3)
-#words.3<-scan_tokenizer(words.3)
+library(triebeard)
 
-# Create probability matrix
-fit.3 <- markovchainFit(data=words.3)
-
-#function for prediction
-inWord="whatever"
-input<-as.vector(unlist(strsplit(inWord," ", fixed=TRUE))) # Input phrase
-p3<-function(n) predict(fit.3$estimate, newdata=as.vector(unlist(strsplit(n," ", fixed=TRUE))), n.ahead = 2)
-p3(input)
+# 2-ngram trie
+trie_2ngram <- trie(keys=as.character(df.2$key), values=as.character(df.2$word))
+# 3-ngram trie
+trie_3ngram <- trie(keys=as.character(df.3$key), values=as.character(df.3$word))
 
 
 
